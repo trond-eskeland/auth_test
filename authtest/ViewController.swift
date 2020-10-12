fa18e2a1d08800da0c76870937c4fc8e4f8662ec
Trond Eskeland <tronni@gmail.com>
2020-10-12 13:44:31 +0200

    Initial Commit

---
+++ authtest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  authtest
+//
+//  Created by Trond Eskeland on 12/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
